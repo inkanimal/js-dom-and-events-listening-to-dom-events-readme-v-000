b69d9eb802b60ed42dc812e1145c69d272a17f83
geoffrey woodcock <theatomproject@gmail.com>
2019-05-30 23:21:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,8 +5,19 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+// const input = document.querySelector('input');
+ 
+// input.addEventListener('keydown', function(e) {
+//   console.log(e.which);
+// });
+
 const input = document.querySelector('input');
  
 input.addEventListener('keydown', function(e) {
-  console.log(e.which);
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
 });
